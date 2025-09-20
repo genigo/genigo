0541413b146ef26edc082b5b37acee876a7d38df
mahmoud <mahmoodeskandari73@gmail.com>
2025-09-20 15:46:19 +0330

    add new line

--- cmd/execute.go
+++ cmd/execute.go
@@ -19,12 +19,12 @@ func Execute() {
 	if len(os.Args) > 1 && strings.ToLower(os.Args[1]) == "init" {
 		initConfig(*conf)
 		fmt.Println("Config file created successfully!")
-		fmt.Printf("Edit [%s] to set database connection and other options", *conf)
+		fmt.Printf("Edit [%s] to set database connection and other options\n", *conf)
 		return
 	}
 
 	if len(os.Args) > 1 && strings.Contains(os.Args[1], "ersion") {
-		fmt.Printf("genigo Ver(%s)\n Developed by Mahmoud Eskandari.", version.Ver)
+		fmt.Printf("genigo Ver(%s)\n Developed by Mahmoud Eskandari.\n", version.Ver)
 		return
 	}
 
@@ -35,12 +35,12 @@ func Execute() {
 
 	err = repo.Connect()
 	if err != nil {
-		log.Fatalf("Error on connecting to database: %+v", err)
+		log.Fatalf("Error on connecting to database: %+v\n", err)
 	}
 
 	err = generator.Generate()
 	if err != nil {
-		log.Fatalf("Error on generate files: %+v", err)
+		log.Fatalf("Error on generate files: %+v\n", err)
 	}
 
 }
